7cdad69638a4607332b007983c4eac457a4c25a1
Yong <neilli1992@gmail.com>
2023-05-18 23:20:56 +0100

    split pdf into chunks

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def main():
     load_dotenv()
@@ -7,6 +9,23 @@ def main():
     st.header("Ask your PDF 📚")
 
     pdf = st.file_uploader("Upload your PDF", type=["pdf"])
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+            )
+        chunks = text_splitter.split_text(text)
+
+        st.write(chunks)
 
 if __name__ == '__main__':
     main()
